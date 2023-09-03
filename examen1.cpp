78a035b5b2a89136f7c008cc4fcbdce3e75d14ac
Gabi0808 <giperez@uamv.edu.ni>
2023-09-03 12:34:53 -0600

    Subiendo el archivo con el cual se va a trabajar

---
+++ examen1.cpp
@@ -0,0 +1,36 @@
+/*1.Se desea eliminar todos los números duplicados de una 
+lista o vector (array). Por ejemplo, si el array
+toma los valores 4 7 11 4 9 5 11 7 3  5 
+ha de cambiarse a 4 7 11 9 5 3.
+A.Escribir una función que elimine los elementos duplicados de un array.
+B.Escribir una función que elimine los elementos duplicados de un vector
+ordenado. ¿Cuál es la eficiencia de esta función?
+2. Se desea realizar un programa que realice las siguientes tareas:
+a) Generar, aleatoriamente, una lista de 999 números enteros en 
+el rango de 0 a 20.000.
+b) Ordenar en modo creciente
+c) Buscar si existe el número x (leído del teclado) 
+en la lista. La búsqueda debe serBinaria*/
+#include <iostream>
+
+void searchDupes(int array[]);
+void deleteDupes(int array[]);
+void deleteDupesOrdered(int array[]);
+void generateRandomNumbers();
+void sortNumbersList(int list[]);
+void binarySearchNumber(char number, int list[]);
+
+
+int main()
+{
+    
+    
+    return 0;
+}
+
+
+
+void deleteDupes(int array[]){
+
+
+}
