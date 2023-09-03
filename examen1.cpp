0ad9021e76fb62c7e67ac72dcbcde22046f35b47
Gabi0808 <giperez@uamv.edu.ni>
2023-09-03 17:37:47 -0600

    Menu mejorado

--- examen1.cpp
+++ examen1.cpp
@@ -18,22 +18,23 @@ en la lista. La búsqueda debe serBinaria*/
 
 using namespace std;
 
-void copyArray(int originArray[], int destinyArray[]);
+void copyArray(int originArray[], int destinyArray[], int size);
 void deleteDupes(int array[]);
 void shellSort(int list[], int n);
-void generateRandomNumbers();
+void generateRandomNumbers(int list[]);
 void bubbleSort(int list[], int n);
 void insertionSort(int list[], int n);
 void exchangeSort(int list[], int n);
 void binarySearchNumber(char number, int list[]);
 void printMenu();
-
+void printFunctionSelected();
 int main()
 {
     int dupedNumbers[10] = {4, 7, 11, 4, 9, 5, 11, 7, 3, 5};
+    int numberList[999], exchangeList[999], bubbleList[999], insertionList[999], shellList[999];
     int deleteNumbersCopy[10], orderedCopy[10];
 
-    int choice;
+    int choice, choice2;
 
     while (true)
     {
@@ -43,58 +44,73 @@ int main()
         switch (choice)
         {
         case 1:
-            copyArray(dupedNumbers, deleteNumbersCopy);
+            copyArray(dupedNumbers, deleteNumbersCopy, 10);
             deleteDupes(deleteNumbersCopy);
             break;
         case 2:
-            generateRandomNumbers();
+            copyArray(dupedNumbers, orderedCopy, 10);
+            shellSort(orderedCopy, 10);
+            deleteDupes(orderedCopy);
             break;
         case 3:
-            copyArray(dupedNumbers, orderedCopy);
-            shellSort(orderedCopy);
-            deleteDupes(orderedCopy);
+
+            generateRandomNumbers(numberList);
+            while (choice2 != 5)
+            {
+                printFunctionSelected();
+                cin >> choice2;
+                switch (choice2)
+                {
+                case 1:
+                    copyArray(numberList, exchangeList, 999);
+                    exchangeSort(exchangeList, 999);
+                    break;
+                case 2:
+                    copyArray(numberList, bubbleList, 999);
+                    bubbleSort(bubbleList, 999);
+                    break;
+                case 3:
+                    copyArray(numberList, insertionList, 999);
+                    insertionSort(insertionList, 999);
+                    break;
+                case 4:
+                    copyArray(numberList, shellList, 999);
+                    shellSort(shellList, 999);
+                    break;
+                case 5:
+                    cout << "Saliendo del programa." << endl;
+                    break;
+                default:
+                    break;
+                }
+            }
+
             break;
         case 4:
             cout << "Saliendo del programa..." << endl;
-            return 0;
+            break;
         default:
             cout << "Opción no válida. Por favor, elige una opción válida." << endl;
             break;
         }
     }
-
     return 0;
 }
 
-int funciones()
+void copyArray(int originArray[], int destinyArray[], int size)
 {
-    int dupedNumbers[10] = {4, 7, 11, 4, 9, 5, 11, 7, 3, 5};
-    int deleteNumbersCopy[10], orderedCopy[10];
-    copyArray(dupedNumbers, deleteNumbersCopy);
-    deleteDupes(deleteNumbersCopy);
-    copyArray(dupedNumbers, orderedCopy);
-    shellSort(orderedCopy, 10);
-    deleteDupes(orderedCopy);
-
-    int shellSortCopy[999], bubbleSortCopy[999], insertionSortCopy[999], exchangeSortCopy[999];
-
-    return 0;
-}
-
-void copyArray(int originArray[], int destinyArray[])
-{
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < size; i++)
     {
         destinyArray[i] = originArray[i];
     }
 }
 
-void deleteDupes(int array[])
+void deleteDupes(int array[], int n)
 {
 
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = i + 1; j < 10; j++)
+        for (int j = i + 1; j < n; j++)
         {
             if (array[i] == array[j])
             {
@@ -107,9 +123,8 @@ void deleteDupes(int array[])
 void shellSort(int list[], int n)
 {
 
-
     int intervalo, i, j, k;
-    intervalo = 10 / 2;
+    intervalo = n / 2;
     while (intervalo > 0)
     {
         for (i = intervalo; i < n; i++)
@@ -134,20 +149,14 @@ void shellSort(int list[], int n)
     }
 }
 
-void generateRandomNumbers()
+void generateRandomNumbers(int list[])
 {
-void generateRandomNumbers()
-{
-
-    int numberList[999];
-    srand(static_cast<unsigned>(time(nullptr)));
-    int numberList[999];
     srand(static_cast<unsigned>(time(nullptr)));
 
     for (int i = 0; i < 999; i++)
     {
         int randomNumber = rand() % 20001;
-        numberList[i] = randomNumber;
+        list[i] = randomNumber;
     }
 }
 
@@ -188,5 +197,34 @@ void insertionSort(int list[], int n)
 void exchangeSort(int list[], int n)
 {
 
+    int i, j;
+    for (i = 0; i <= n - 2; i++)
 
+        for (j = i + 1; j <= n - 1; j++)
+            if (list[i] > list[j])
+            {
+                int aux;
+                aux = list[i];
+                list[i] = list[j];
+                list[j] = aux;
+            }
+}
+
+void printMenu()
+{
+    cout << "Menu examen 1er corte II Semestre" << endl;
+    cout << "Seleccione una opcion: " << endl;
+    cout << "1. Eliminar numeros duplicados en un arreglo desordenado." << endl;
+    cout << "2. Eliminar numeros duplicados en un arreglo ordenado." << endl;
+    cout << "3. Ordenamiento de una lista de numeros aleatorios." << endl;
+    cout << "4. Salir del programa" << endl;
+}
+void printFunctionSelected()
+{
+    cout << "Con que metodo desea ordenar los numeros?" << endl;
+    cout << "1. Ordenar lista de numeros aleatorios por intercambio." << endl;
+    cout << "2. Ordenar lista de numeros aleatorios por metodo burbuja." << endl;
+    cout << "3. Ordenar lista de numeros aleatorios por insercion." << endl;
+    cout << "4. Ordenar lista de numeros aleatorios por metodo shell." << endl;
+    cout << "5. Salir del menu. " << endl;
 }
