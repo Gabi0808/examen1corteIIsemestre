92c009cf29e75e0881492ba02197095d8245ac45
Gabi0808 <giperez@uamv.edu.ni>
2023-09-03 19:44:27 -0600

    Codigo funcional

--- examen1.cpp
+++ examen1.cpp
@@ -6,15 +6,16 @@ A.Escribir una función que elimine los elementos duplicados de un array.
 B.Escribir una función que elimine los elementos duplicados de un vector
 ordenado. ¿Cuál es la eficiencia de esta función?
 2. Se desea realizar un programa que realice las siguientes tareas:
-a) Generar, aleatoriamente, una lista de 999 números enteros en
+a) Generar, aleatoriamente, una lista de MAX números enteros en
 el rango de 0 a 20.000.
 b) Ordenar en modo creciente
 c) Buscar si existe el número x (leído del teclado)
 en la lista. La búsqueda debe serBinaria*/
 #include <iostream>
-#define MAX 10
+#define MAX 999
 #include <cstdlib>
 #include <ctime>
+#include <chrono>
 
 using namespace std;
 
@@ -25,18 +26,21 @@ void generateRandomNumbers(int list[]);
 void bubbleSort(int list[], int n);
 void insertionSort(int list[], int n);
 void exchangeSort(int list[], int n);
-void binarySearchNumber(char number, int list[]);
+int binarySearchNumber(int key, int list[], int size);
 void printMenu();
+void printArray(int array[], int size);
 void printFunctionSelected();
+
 int main()
 {
     int dupedNumbers[10] = {4, 7, 11, 4, 9, 5, 11, 7, 3, 5};
-    int numberList[999], exchangeList[999], bubbleList[999], insertionList[999], shellList[999];
+    int numberList[MAX], exchangeList[MAX], bubbleList[MAX], insertionList[MAX], shellList[MAX];
     int deleteNumbersCopy[10], orderedCopy[10];
 
-    int choice, choice2;
-
-    while (choice!=4)
+    int choice, choice2, searchedNumber, results;
+    unsigned t0, t1;
+    t0 = clock();
+    while (choice != 4)
     {
         printMenu();
         cin >> choice;
@@ -55,32 +59,57 @@ int main()
         case 3:
 
             generateRandomNumbers(numberList);
-            while (choice2 != 5)
+            while (choice2 != 6)
             {
                 printFunctionSelected();
                 cin >> choice2;
                 switch (choice2)
                 {
                 case 1:
-                    copyArray(numberList, exchangeList, 999);
-                    exchangeSort(exchangeList, 999);
+                    copyArray(numberList, exchangeList, MAX);
+                    exchangeSort(exchangeList, MAX);
+                    printArray(exchangeList, MAX);
+                    exchangeSort(exchangeList, MAX);
                     break;
                 case 2:
-                    copyArray(numberList, bubbleList, 999);
-                    bubbleSort(bubbleList, 999);
+                    copyArray(numberList, bubbleList, MAX);
+                    bubbleSort(bubbleList, MAX);
+                    printArray(bubbleList, MAX);
+                    bubbleSort(bubbleList, MAX);
                     break;
                 case 3:
-                    copyArray(numberList, insertionList, 999);
-                    insertionSort(insertionList, 999);
+                    copyArray(numberList, insertionList, MAX);
+                    insertionSort(insertionList, MAX);
+                    printArray(insertionList, MAX);
+                    insertionSort(insertionList, MAX);
                     break;
                 case 4:
-                    copyArray(numberList, shellList, 999);
-                    shellSort(shellList, 999);
+                    copyArray(numberList, shellList, MAX);
+                    shellSort(shellList, MAX);
+                    printArray(shellList, MAX);
+                    shellSort(shellList, MAX);
                     break;
                 case 5:
+                    printArray(numberList, MAX);
+                    break;
+                case 6:
+                    cout << "Ingresa el numero a buscar" << endl;
+                    cin >> searchedNumber;
+                    shellSort(shellList, MAX);
+                    results = binarySearchNumber(searchedNumber, shellList, MAX);
+                    if (results == -1)
+                    {
+                        cout << "No se encontro el elemento en la lista" << endl;
+                        break;
+                    }
+                    cout << "El numero se encuentra en la lista, en el indice: " << results << endl;
+
+                    break;
+                case 7:
                     cout << "Saliendo del programa." << endl;
                     break;
                 default:
+                    cout << "Ingrese una opcion valida." << endl;
                     break;
                 }
             }
@@ -94,6 +123,9 @@ int main()
             break;
         }
     }
+    t1 = clock();
+    double time = (double(t0 - t1) / CLOCKS_PER_SEC);
+    cout << "El tiempo transcurrido es de " << time << endl;
     return 0;
 }
 
@@ -104,7 +136,6 @@ void copyArray(int originArray[], int destinyArray[], int size)
         destinyArray[i] = originArray[i];
     }
 }
-
 void deleteDupes(int array[], int n)
 {
 
@@ -119,10 +150,9 @@ void deleteDupes(int array[], int n)
         }
     }
 }
-
 void shellSort(int list[], int n)
 {
-
+    auto inicio = chrono::high_resolution_clock::now();
     int intervalo, i, j, k;
     intervalo = n / 2;
     while (intervalo > 0)
@@ -147,21 +177,25 @@ void shellSort(int list[], int n)
         }
         intervalo = intervalo / 2;
     }
+    auto fin = chrono::high_resolution_clock::now();
+    chrono::duration<double, nano> tiempo_promedio = fin - inicio;
+    cout << "Tiempo promedio de ejecución: " << tiempo_promedio.count() << " nanosegundos" << endl;
 }
 
 void generateRandomNumbers(int list[])
 {
     srand(static_cast<unsigned>(time(nullptr)));
 
-    for (int i = 0; i < 999; i++)
+    for (int i = 0; i < MAX; i++)
     {
         int randomNumber = rand() % 20001;
         list[i] = randomNumber;
     }
 }
-
 void bubbleSort(int list[], int n)
 {
+
+    auto inicio = chrono::high_resolution_clock::now();
     int interruptor = 1;
     int pasada, j;
     for (pasada = 0; pasada < n - 1 && interruptor; pasada++)
@@ -177,9 +211,13 @@ void bubbleSort(int list[], int n)
                 list[j + 1] = aux;
             }
     }
+    auto fin = chrono::high_resolution_clock::now();
+    chrono::duration<double, nano> tiempo_promedio = fin - inicio;
+    cout << "Tiempo promedio de ejecución: " << tiempo_promedio.count() << " nanosegundos" << endl;
 }
 void insertionSort(int list[], int n)
 {
+    auto inicio = chrono::high_resolution_clock::now();
     int i, j;
     int aux;
     for (i = 1; i < n; i++)
@@ -193,10 +231,13 @@ void insertionSort(int list[], int n)
         }
         list[j] = aux;
     }
+    auto fin = chrono::high_resolution_clock::now();
+    chrono::duration<double, nano> tiempo_promedio = fin - inicio;
+    cout << "Tiempo promedio de ejecución: " << tiempo_promedio.count() << " nanosegundos" << endl;
 }
 void exchangeSort(int list[], int n)
 {
-
+    auto inicio = chrono::high_resolution_clock::now();
     int i, j;
     for (i = 0; i <= n - 2; i++)
 
@@ -208,8 +249,10 @@ void exchangeSort(int list[], int n)
                 list[i] = list[j];
                 list[j] = aux;
             }
+    auto fin = chrono::high_resolution_clock::now();
+    chrono::duration<double, nano> tiempo_promedio = fin - inicio;
+    cout << "Tiempo promedio de ejecución: " << tiempo_promedio.count() << " nanosegundos" << endl;
 }
-
 void printMenu()
 {
     cout << "Menu examen 1er corte II Semestre" << endl;
@@ -226,5 +269,40 @@ void printFunctionSelected()
     cout << "2. Ordenar lista de numeros aleatorios por metodo burbuja." << endl;
     cout << "3. Ordenar lista de numeros aleatorios por insercion." << endl;
     cout << "4. Ordenar lista de numeros aleatorios por metodo shell." << endl;
-    cout << "5. Salir del menu. " << endl;
+    cout << "5. Mostrar lista de numeros aleatorios sin ordenar. " << endl;
+    cout << "6. Realizar busqueda binaria." << endl;
+    cout << "7. Salir del menu." << endl;
+}
+void printArray(int array[], int size)
+{
+
+    cout << "Mostrando el arreglo: " << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << array[i] << endl;
+    }
+}
+
+int binarySearchNumber(int key, int list[], int size)
+{
+    auto inicio = chrono::high_resolution_clock::now();
+    int central, bajo, alto;
+    int valorCentral;
+    bajo = 0;
+    alto = size - 1;
+    while (bajo <= alto)
+    {
+        central = (bajo + alto) / 2;
+        valorCentral = list[central];
+        if (key == valorCentral)
+            return central;
+        else if (key < valorCentral)
+            alto = central - 1;
+        else
+            bajo = central + 1;
+    }
+    auto fin = chrono::high_resolution_clock::now();
+    chrono::duration<double, nano> tiempo_promedio = fin - inicio;
+    cout << "Tiempo promedio de ejecución: " << tiempo_promedio.count() << " nanosegundos" << endl;
+    return -1;
 }
